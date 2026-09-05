7d07651ba294b62f48f6815ad047cb2908540c16
Raman Sunder <ramansunder@gmail.com>
2026-09-05 18:58:55 +0530

    9-5-26

--- src/components/Authentication/Register.jsx
+++ src/components/Authentication/Register.jsx
@@ -7,8 +7,8 @@ export default function Register() {
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [contact, setContact] = useState("");
-  const [userType, setUserType] = useState('')
-  const [institutionId, setInstitutionId] = useState('')
+  const [userType, setUserType] = useState("");
+  const [institutionId, setInstitutionId] = useState("");
 
   const nav = useNavigate();
 
@@ -17,7 +17,14 @@ export default function Register() {
 
     let pattern = /^[6-9]{1}[0-9]{9}$/;
 
-    if (!name || !email || !password || !contact || !userType || !institutionId) {
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !contact ||
+      !userType ||
+      !institutionId
+    ) {
       toast.error("All fields are required");
       return;
     }
@@ -31,17 +38,16 @@ export default function Register() {
         email: email,
         password: password,
         contact: contact,
-        userType : userType,
-        institutionId : institutionId
+        userType: userType,
+        institutionId: institutionId,
       };
       await Registerservices.Register(payload);
       toast.success("Registered");
-
     } catch (error) {
       switch (error.code) {
         case "auth/email-already-in-use":
           toast.error("Email is already registered");
-          
+
           break;
 
         case "auth/invalid-email":
@@ -100,85 +106,79 @@ export default function Register() {
                     >
                       <div className="bg-secondary rounded p-5 w-50">
                         <h1 className="text-white mb-4">Register</h1>
-                        <form onSubmit={Register}>
-                          <div className="row g-3">
-                            <div className="col-12">
-                              <input
-                                className="w-100 fs-4"
-                                type="email"
-                                placeholder="Enter Your Email"
-                                onChange={(e) => setEmail(e.target.value)}
-                              />
+                        <form>
+                          <div className="row g-4">
+                            <div className="col-lg-12 col-xl-6">
+                              <div className="form-floating">
+                                <input
+                                  type="email"
+                                  className="form-control"
+                                  id="email"
+                                  placeholder="Your Email"
+                                />
+                                <label htmlFor="email">Your Email</label>
+                              </div>
+                            </div>
+                            <div className="col-lg-12 col-xl-6">
+                              <div className="form-floating">
+                                <input
+                                  type="text"
+                                  className="form-control"
+                                  id="name"
+                                  placeholder="Your Name"
+                                />
+                                <label htmlFor="name">Your Name</label>
+                              </div>
+                            </div>
+                            <div className="col-lg-12 col-xl-6">
+                              <div className="form-floating">
+                                <input
+                                  type="password"
+                                  className="form-control"
+                                  id="password"
+                                  placeholder="Phone"
+                                />
+                                <label htmlFor="phone">Your password</label>
+                              </div>
+                            </div>
+                            <div className="col-lg-12 col-xl-6">
+                              <div className="form-floating">
+                                <input
+                                  type="text"
+                                  className="form-control"
+                                  id="project"
+                                  placeholder="Project"
+                                />
+                                <label htmlFor="project">Your Project</label>
+                              </div>
                             </div>
                             <div className="col-12">
-                              <input
-                                className="w-100 fs-4"
-                                type="password"
-                                placeholder="Enter Password"
-                                onChange={(e) => setPassword(e.target.value)}
-                              />
+                              <div className="form-floating">
+                                <input
+                                  type="text"
+                                  className="form-control"
+                                  id="subject"
+                                  placeholder="Subject"
+                                />
+                                <label htmlFor="subject">Subject</label>
+                              </div>
                             </div>
-
                             <div className="col-12">
-                              <input
-                                className="w-100 fs-4"
-                                type="text"
-                                placeholder="Enter Name"
-                                onChange={(e) => setName(e.target.value)}
-                              />
+                              <div className="form-floating">
+                                <textarea
+                                  className="form-control"
+                                  placeholder="Leave a message here"
+                                  id="message"
+                                  style={{ height: 160 }}
+                                  defaultValue={""}
+                                />
+                                <label htmlFor="message">Message</label>
+                              </div>
                             </div>
-
                             <div className="col-12">
-                              <input
-                                className="w-100 fs-4"
-                                type="text"
-                                placeholder="Enter InstituitonId"
-                                onChange={(e) => setInstitutionId(e.target.value)}
-                              />
-                            </div>
-
-                            <div className="col-12">
-                              <input
-                                className="w-100 fs-4"
-                                type="number"
-                                placeholder="Enter Contact"
-                                onChange={(e) => setContact(e.target.value)}
-                              />
-                            </div>
-
-                            <div className="col-12">
-                              <select
-                                value={userType}
-                                onChange={(e) => setUserType(e.target.value)}
-                                className="fs-4"
-                              >
-                                <option value="">Select User Type</option>
-                                <option value="admin">Admin</option>
-                                <option value="user">Student</option>
-                              </select>
-                            </div>
-
-                            <div className="col-12">
-                              <button
-                                className="btn btn-light w-100 py-2"
-                                type="submit"
-                              >
-                                Register
+                              <button className="btn btn-light w-100 py-3">
+                                Send Message
                               </button>
-                            </div>
-
-                            <h4 className="text-white">
-                              Already have an account?
-                            </h4>
-                            <div className="col-12">
-                              <Link to="/login">
-                                <button
-                                  className="btn btn-light w-100 py-2"
-                                  type="button"
-                                >
-                                  Login
-                                </button>
-                              </Link>
                             </div>
                           </div>
                         </form>
--- src/components/user/manageprofile/Manageprofile.jsx
+++ src/components/user/manageprofile/Manageprofile.jsx
@@ -1,19 +1,15 @@
-
-
-
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
 // import Registerservices from "../../services/Registerservices";
-
 
 export default function Manageprofile() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [contact, setContact] = useState("");
-  const [userType, setUserType] = useState('')
-  const [institutionId, setInstitutionId] = useState('')
+  const [userType, setUserType] = useState("");
+  const [institutionId, setInstitutionId] = useState("");
 
   const nav = useNavigate();
 
@@ -26,27 +22,28 @@ export default function Manageprofile() {
       toast.error("All fields are required");
       return;
     }
+
     if (!pattern.test(contact)) {
       toast.error("Please enter a valid number");
       return;
     }
+
     try {
       let payload = {
         name: name,
         email: email,
         password: password,
         contact: contact,
-        userType : userType,
-        institutionId : institutionId
+        userType: userType,
+        institutionId: institutionId,
       };
-      await Registerservices.Register(payload);
-      toast.success("Registered");
 
+      // await Registerservices.Register(payload);
+      toast.success("Registered");
     } catch (error) {
       switch (error.code) {
         case "auth/email-already-in-use":
           toast.error("Email is already registered");
-          
           break;
 
         case "auth/invalid-email":
@@ -63,105 +60,187 @@ export default function Manageprofile() {
 
         default:
           toast.error(error.message);
-        // console.log(error);
       }
     }
   }
 
   return (
     <>
+      {/* Page Heading */}
       <div className="container-fluid bg-breadcrumb">
-          <div className="container text-center py-5" style={{ maxWidth: 900 }}>
-            <h4
-              className="text-white display-4 mb-4 wow fadeInDown"
-              data-wow-delay="0.1s"
-            >My Profile
-            </h4>
-          </div>
-        </div>
-
-
-
-      <div className=" container vh-100 d-flex justify-content-center align-items-center col-xl-6 wow fadeInUp" data-wow-delay="0.1s">
-  <div className="bg-secondary p-5 rounded">
-    <h4 className="text-primary mb-4">Edit Profile</h4>
-    <form>
-      <div className="row g-4">
-        <div className="col-lg-12 col-xl-6">
-          <div className="form-floating">
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Your Email"
-            />
-            <label htmlFor="email">Your Email</label>
-          </div>
-        </div>
-        <div className="col-lg-12 col-xl-6">
-          <div className="form-floating">
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              placeholder="Your Name"
-            />
-            <label htmlFor="name">Your Name</label>
-          </div>
-        </div>
-        <div className="col-lg-12 col-xl-6">
-          <div className="form-floating">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Phone"
-            />
-            <label htmlFor="phone">Your password</label>
-          </div>
-        </div>
-        <div className="col-lg-12 col-xl-6">
-          <div className="form-floating">
-            <input
-              type="text"
-              className="form-control"
-              id="project"
-              placeholder="Project"
-            />
-            <label htmlFor="project">Your Project</label>
-          </div>
-        </div>
-        <div className="col-12">
-          <div className="form-floating">
-            <input
-              type="text"
-              className="form-control"
-              id="subject"
-              placeholder="Subject"
-            />
-            <label htmlFor="subject">Subject</label>
-          </div>
-        </div>
-        <div className="col-12">
-          <div className="form-floating">
-            <textarea
-              className="form-control"
-              placeholder="Leave a message here"
-              id="message"
-              style={{ height: 160 }}
-              defaultValue={""}
-            />
-            <label htmlFor="message">Message</label>
-          </div>
-        </div>
-        <div className="col-12">
-          <button className="btn btn-light w-100 py-3">Send Message</button>
+        <div
+          className="container text-center py-5"
+          style={{ maxWidth: 900 }}
+        >
+          <h4
+            className="text-white display-4 mb-4 wow fadeInDown"
+            data-wow-delay="0.1s"
+          >
+            My Profile
+          </h4>
         </div>
       </div>
-    </form>
-  </div>
-</div>
 
+      {/* Image + Form Section */}
+      <div
+        className="profile-section"
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100vh",
+          overflow: "hidden",
+        }}
+      >
+        {/* Background Image */}
+        <img
+          src="/img/red-and-white-background.jpg"
+          alt="Profile Background"
+          style={{
+            position: "absolute",
+            // top: 0,
+            // left: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+
+        {/* Edit Profile Form */}
+        <div
+          className="bg-secondary rounded container d-flex justify-content-center align-items-center w-50"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            // top: "50%",
+            // right: "6%",
+            // transform: "translateY(-50%)",
+            // width: "360px",
+            // maxWidth: "90%",
+            // zIndex: 2,
+          }}
+        >
+          <form >
+          <h4
+            className="mb-4 "
+            style={{
+              color: "red",
+              fontSize: "28px",
+              fontWeight: "400",
+            }}
+          >
+            Edit Profile
+          </h4>
+
+          
+            <div className="row g-3 w-70">
+
+              {/* Email */}
+              <div className="col-12 col-xl-6">
+                <div className="form-floating">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder="Your Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <label htmlFor="email">Your Email</label>
+                </div>
+              </div>
+
+              {/* Name */}
+              <div className="col-12 col-xl-6">
+                <div className="form-floating">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    placeholder="Your Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <label htmlFor="name">Your Name</label>
+                </div>
+              </div>
+
+              {/* Password */}
+              <div className="col-12 col-xl-6">
+                <div className="form-floating">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Your Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <label htmlFor="password">Your Password</label>
+                </div>
+              </div>
+
+              {/* Project */}
+              <div className="col-12 col-xl-6">
+                <div className="form-floating">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="project"
+                    placeholder="Your Project"
+                  />
+                  <label htmlFor="project">Your Project</label>
+                </div>
+              </div>
+
+              {/* Subject */}
+              <div className="col-12">
+                <div className="form-floating">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="subject"
+                    placeholder="Subject"
+                  />
+                  <label htmlFor="subject">Subject</label>
+                </div>
+              </div>
+
+              {/* Message */}
+              <div className="col-12">
+                <div className="form-floating">
+                  <textarea
+                    className="form-control"
+                    placeholder="Leave a message here"
+                    id="message"
+                    style={{ height: "160px" }}
+                  ></textarea>
+
+                  <label htmlFor="message">Message</label>
+                </div>
+              </div>
+
+              {/* Button */}
+              <div className="col-12">
+                <button
+                  type="submit"
+                  className="btn btn-light w-100 py-3"
+                  style={{
+                    color: "red",
+                    fontWeight: "500",
+                  }}
+                >
+                  Send Message
+                </button>
+              </div>
+
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
